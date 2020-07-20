0558d3d5307216056d52224d889c220fa78ade4e
Robert Läßig <r.laessig@posteo.de>
2020-07-20 13:39:04 +0200

    added dirty support for batch processing

--- Vision_API/vision_request.py
+++ Vision_API/vision_request.py
@@ -6,62 +6,102 @@ import json
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
+from google.cloud.vision_v1 import enums
 
 from google.protobuf.json_format import MessageToDict
 
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.abspath(sys.argv[1])
+# The name of the image file to annotate or the path to the folder with the images to batch annotate
+source = os.path.abspath(sys.argv[1])
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+if(os.path.isdir(source)):
+	
+	files = []
+	# r=root, d=directories, f = files
+	for r, d, f in os.walk(source):
+		for file in f:
+				if not '.DS_Store' in file:
+					files.append(os.path.join(r, file))
 
-image = types.Image(content=content)
+	features = [
+		{"type": enums.Feature.Type.LABEL_DETECTION},
+		{"type": enums.Feature.Type.OBJECT_LOCALIZATION},
+	]
 
-# Performs label detection on the image file
-response_labels = client.label_detection(image=image,max_results=100)
-labels = response_labels.label_annotations
-response_objects = client.object_localization(image=image,max_results=100)
-objects = response_objects.localized_object_annotations
+	requests = []
+	for f in files:
+		with io.open(f, 'rb') as image_file:
+			content = image_file.read()
+		requests.append({"image": types.Image(content=content), "features": features})
 
-label_output = []
-object_output = []
+	response = client.batch_annotate_images(requests)
 
-for label in labels:
-	label_output.append({
-		'description': label.description,
-		'score': label.score
-		})
+	response = MessageToDict(response)
 
-for localized_object in objects:
-	object_output.append({
-		'description': localized_object.name,
-		'score': localized_object.score
-		})
+	# remove file extension from image file path
+	filename = os.path.splitext(sys.argv[1])[0]
 
-#construct json to generate easily readable output
-output = {
-	'labels' : label_output,
-	'objects': object_output
-}
+	for i, f in enumerate(files):
+		#write request json to path where img is located
+		filename = os.path.splitext(f)[0]
+		with open(filename + '-origin.json', 'w') as outfile:
+			json.dump(response['responses'][i], outfile, indent=4)
 
-# remove file extension from image file path
-filename = os.path.splitext(sys.argv[1])[0]
+else :
 
-#write request json to path where img is located
-with open(filename + '.json', 'w') as outfile:
-    json.dump(output, outfile, indent=2)
-
-#generating full output to have positions of recognized objects included
-
-output = {
-	'labelAnnotations' : MessageToDict(response_labels),
-	'localizedObjectAnnotations': MessageToDict(response_objects)
-}
-
-#write request json to path where img is located
-with open(filename + '-origin.json', 'w') as outfile:
-    json.dump(output, outfile, indent=2)
+	
+	
+	# Loads the image into memory
+	with io.open(file_name, 'rb') as image_file:
+	    content = image_file.read()
+	
+	image = types.Image(content=content)
+	
+	# Performs label detection on the image file
+	response_labels = client.label_detection(image=image,max_results=100)
+	labels = response_labels.label_annotations
+	response_objects = client.object_localization(image=image,max_results=100)
+	objects = response_objects.localized_object_annotations
+	
+	label_output = []
+	object_output = []
+	
+	for label in labels:
+		label_output.append({
+			'description': label.description,
+			'score': label.score
+			})
+	
+	for localized_object in objects:
+		object_output.append({
+			'description': localized_object.name,
+			'score': localized_object.score
+			})
+	
+	#construct json to generate easily readable output
+	output = {
+		'labels' : label_output,
+		'objects': object_output
+	}
+	
+	# remove file extension from image file path
+	filename = os.path.splitext(sys.argv[1])[0]
+	
+	#write request json to path where img is located
+	with open(filename + '.json', 'w') as outfile:
+	    json.dump(output, outfile, indent=2)
+	
+	#generating full output to have positions of recognized objects included
+	
+	output = {
+		'labelAnnotations' : MessageToDict(response_labels),
+		'localizedObjectAnnotations': MessageToDict(response_objects)
+	}
+	
+	#write request json to path where img is located
+	with open(filename + '-origin.json', 'w') as outfile:
+	    json.dump(output, outfile, indent=2)
+	
+	
