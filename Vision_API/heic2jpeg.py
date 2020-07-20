502d64e5444003c0dd7318325aab2b48332e1e35
Robert Läßig <r.laessig@posteo.de>
2020-07-20 22:26:12 +0200

    made scripts reusable in other scripts

--- Vision_API/heic2jpeg.py
+++ Vision_API/heic2jpeg.py
@@ -6,24 +6,33 @@ import sys
 # The name of the image file to annotate or the path to the folder with the images to batch annotate
 source = os.path.abspath(sys.argv[1])
 
-if(os.path.isdir(source)):
-	
-	files = []
-	# r=root, d=directories, f = files
-	for r, d, f in os.walk(source):
-		for file in f:
-				if not '.DS_Store' in file and not '.json' in file and not '.localized' in file:
-					files.append(os.path.join(r, file))
-	
+def heic2jpeg(source):
+	if (os.path.isdir(source)):
+		
+		files = []
+		# r=root, d=directories, f = files
+		for r, d, f in os.walk(source):
+			for file in f:
+					if not '.DS_Store' in file and not '.json' in file and not '.localized' in file:
+						files.append(os.path.join(r, file))
+	else:
+		files = [source]
+		
 	for f in files:
 		heif_file = pyheif.read(f)
 		image = Image.frombytes(
-    		heif_file.mode, 
-    		heif_file.size, 
-    		heif_file.data,
-    		"raw",
-    		heif_file.mode,
-    		heif_file.stride,
-    		)
+			heif_file.mode, 
+			heif_file.size, 
+			heif_file.data,
+			"raw",
+			heif_file.mode,
+			heif_file.stride,
+			)
 		filename = os.path.splitext(f)[0]
 		image.save(filename + ".jpg", "JPEG")
+		os.remove(f)
+
+if __name__ == "__main__":
+	# The name of the image file to annotate or the path to the folder with the images to batch annotate
+	source = os.path.abspath(sys.argv[1])
+	heic2jpeg(source)
--- Vision_API/vision_request.py
+++ Vision_API/vision_request.py
@@ -10,69 +10,73 @@ from google.cloud.vision_v1 import enums
 
 from google.protobuf.json_format import MessageToDict
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def request_annotations(source):
+	# Instantiates a client
+	client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate or the path to the folder with the images to batch annotate
-source = os.path.abspath(sys.argv[1])
+	#get all files from the given folder
+	if(os.path.isdir(source)):
+		
+		files = []
+		# r=root, d=directories, f = files
+		for r, d, f in os.walk(source):
+			for file in f:
+					if not '.DS_Store' in file and not '.json' in file and not '.localized' in file:
+						files.append(os.path.join(r, file))
 
-#get all files from the given folder
-if(os.path.isdir(source)):
-	
-	files = []
-	# r=root, d=directories, f = files
-	for r, d, f in os.walk(source):
-		for file in f:
-				if not '.DS_Store' in file and not '.json' in file and not '.localized' in file:
-					files.append(os.path.join(r, file))
+	#if only one img is given save it in the same array structure when many are given
+	else :
 
-#if only one img is given save it in the same array structure when many are given
-else :
+		files = [source]
 
-	files = [source]
+	#define features to retrieve from the API
+	features = [
+		{"type": enums.Feature.Type.LABEL_DETECTION},
+		{"type": enums.Feature.Type.OBJECT_LOCALIZATION},
+	]
 
-#define features to retrieve from the API
-features = [
-	{"type": enums.Feature.Type.LABEL_DETECTION},
-	{"type": enums.Feature.Type.OBJECT_LOCALIZATION},
-]
+	#build requests
+	requests = []
+	for f in files:
+		with io.open(f, 'rb') as image_file:
+			content = image_file.read()
+		requests.append({"image": types.Image(content=content), "features": features})
 
-#build requests
-requests = []
-for f in files:
-	with io.open(f, 'rb') as image_file:
-		content = image_file.read()
-	requests.append({"image": types.Image(content=content), "features": features})
+	#make request
+	response = client.batch_annotate_images(requests)
 
-#make request
-response = client.batch_annotate_images(requests)
+	#transform request to dict
+	response = MessageToDict(response)
 
-#transform request to dict
-response = MessageToDict(response)
+	#output response as json
+	for i, f in enumerate(files):
 
-#output response as json
-for i, f in enumerate(files):
+		#write full response json to path where img is located
+		filename = os.path.splitext(f)[0]
+		with open(filename + '-origin.json', 'w') as outfile:
+			json.dump(response['responses'][i], outfile, indent=4)
 
-	#write full response json to path where img is located
-	filename = os.path.splitext(f)[0]
-	with open(filename + '-origin.json', 'w') as outfile:
-		json.dump(response['responses'][i], outfile, indent=4)
+		#write simplifide response json to path where img is located
+		labels = []
+		objects = []
 
-	#write simplifide response json to path where img is located
-	labels = []
-	objects = []
+		if 'labelAnnotations' in response['responses'][i]:
+			for annotation in response['responses'][i]['labelAnnotations']:
+				labels.append(annotation['description'] + ' (' + str(annotation['score']) + ')')
+		if 'localizedObjectAnnotations' in response['responses'][i]:
+			for annotation in response['responses'][i]['localizedObjectAnnotations']:
+				objects.append(annotation['name'] + ' (' + str(annotation['score']) + ')')
 
-	if 'labelAnnotations' in response['responses'][i]:
-		for annotation in response['responses'][i]['labelAnnotations']:
-			labels.append(annotation['description'] + ' (' + str(annotation['score']) + ')')
-	if 'localizedObjectAnnotations' in response['responses'][i]:
-		for annotation in response['responses'][i]['localizedObjectAnnotations']:
-			objects.append(annotation['name'] + ' (' + str(annotation['score']) + ')')
+		output = {
+			'labels' : labels,
+			'objects': objects
+		}		
 
-	output = {
-		'labels' : labels,
-		'objects': objects
-	}		
+		with open(filename + '-readable.json', 'w') as outfile:
+			json.dump(output, outfile, indent=4)
 
-	with open(filename + '-readable.json', 'w') as outfile:
-		json.dump(output, outfile, indent=4)
+if __name__ == "__main__":
+	# The name of the image file to annotate or the path to the folder with the images to batch annotate
+	source = os.path.abspath(sys.argv[1])
+	request_annotations(source)
+
